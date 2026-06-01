d51e798e98eb6fab91ae808e2928b7a50897fd5c
lucca ruchichi <luccaiiix@gmail.com>
2026-06-01 16:40:45 -0400

    fix subtitle typography3

--- app.py
+++ app.py
@@ -561,8 +561,8 @@ def plantilla_noticia(datos, imagen):
             fill=(255,255,255),
             stroke_width=2,
             stroke_fill=(0,0,0)
-            )
-            y += 65
+        )
+        y += 65
 
     d.rectangle((55, 1285, 1025, 1292), fill=MORADO)
     return img
